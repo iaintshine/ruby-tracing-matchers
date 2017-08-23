26342b5ee816d85315dcf7d790cfd439773f705a
iaintshine <bodziomista@gmail.com>
2017-08-23 22:58:18 +0200

    Remove failing test

--- spec/tracing/matchers_spec.rb
+++ spec/tracing/matchers_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Tracing::Matchers do
   it "has a version number" do
     expect(Tracing::Matchers::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
